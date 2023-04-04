71b3e51a6e84b2eea4e7b6fc75ace70bfa821e99
Isaac Ojeda <isaac.ojeda@intelectix.com>
2023-04-04 14:52:21 -0700

    Distributed cache example

---
+++ DistributedCacheExample/Api02/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Api02.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IDistributedCache _distributedCache;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IDistributedCache distributedCache)
+    {
+        _logger = logger;
+        _distributedCache = distributedCache;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    [ResponseCache(Duration = 30)]
+    public async Task<IEnumerable<WeatherForecast>> Get()
+    {
+        var dataInBytes = await _distributedCache.GetAsync("GetWeatherForecast");
+        var data = Enumerable.Empty<WeatherForecast>();
+
+        if (dataInBytes is null)
+        {
+            data = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            var dataJson = JsonSerializer.Serialize(data);
+            dataInBytes = System.Text.Encoding.UTF8.GetBytes(dataJson);
+
+            _distributedCache.Set("GetWeatherForecast", dataInBytes, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(5)
+            });
+        }
+
+        var rawJson = System.Text.Encoding.UTF8.GetString(dataInBytes);
+        data = JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(rawJson);
+
+        return data;
+    }
+}
